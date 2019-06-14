53cc67ab1e9cfc3f5a8e991670231dd801ff0ced
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2019-06-14 19:35:33 +0300

    -

--- loop.rb
+++ loop.rb
@@ -65,8 +65,8 @@ class Loop
   def run
 
     loop do
-      tl = @lower_motor.go_to(pos: L1, max_velocity: VELOCITY / 2, acceleration: ACCELERATION, start_immediately: false)
-      tu = @upper_motor.go_to(pos: U1, max_velocity: VELOCITY / 2, acceleration: ACCELERATION, start_immediately: false)
+      tl = @lower_motor.go_to(pos: L1, max_velocity: VELOCITY, acceleration: ACCELERATION, start_immediately: false)
+      tu = @upper_motor.go_to(pos: U1, max_velocity: VELOCITY, acceleration: ACCELERATION, start_immediately: false)
       @servo_interface.start_motion
       sleep [tl / 1000, tu / 1000].max + 0.5
       # p [@lower_motor.position, @upper_motor.position]
